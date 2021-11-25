a5bbc28ccd3035c87feadeaa41b46455e3411067
KholodH <91002535+KholodH@users.noreply.github.com>
2021-11-26 01:16:09 +0300

    Create suits.swift

---
+++ suits.swift
@@ -0,0 +1,11 @@
+let suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+var deckOfCards = [String: [Int]]()
+// your code here
+print(deckOfCards)
+
+// Given the following three variables write a for-in loop that will have the following output in the Assistant Editor.
+for suit in suits{
+deckOfCards[suit] = cards
+}
+print (deckOfCards)
